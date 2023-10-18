02cc7b0cc2cc470f9a92b589cedf61ff3ca9a047
Adonis Almonte <dev.adonis.almonte@gmail.com>
2023-10-18 15:22:11 -0400

    Creating keys

--- fcrypt.py
+++ fcrypt.py
@@ -1,5 +1,16 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+data = "Hello World!"
+
+sender_private_key = RSA.generate(2048)
+sender_public_key = sender_private_key.public_key()
+
+receiver_private_key = RSA.generate(2048)
+receiver_public_key = receiver_private_key.public_key()
+
 print("Hello")
 print("Hello World")
 print("Bye")
 
-# this is the start of our project on GitHub! - Dang
+# this is a comment by Dang!
